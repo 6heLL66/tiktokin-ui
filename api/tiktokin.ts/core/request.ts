05d4d9ec5a75b9cc436d8fccaab5bac62f2bccd0
Никита Семенчук <n.semenchuk@texture.finance>
2025-06-04 21:59:35 +0300

    asd

--- api/tiktokin.ts/core/request.ts
+++ api/tiktokin.ts/core/request.ts
@@ -42,7 +42,6 @@ export const base64 = (str: string): string => {
     try {
         return btoa(str);
     } catch (err) {
-        // @ts-expect-error
         return Buffer.from(str).toString('base64');
     }
 };
