614648ee127ebc27d429de4fa660e938648581c0
Никита Семенчук <n.semenchuk@texture.finance>
2025-06-05 17:03:49 +0300

    asd

--- features/useTiktok.ts
+++ features/useTiktok.ts
@@ -4,7 +4,7 @@ export const useTiktok = (url?: string) => {
     const query = useQuery({
         queryKey: ['tiktok', url],
         queryFn: () => {
-            return fetch(`http://www.tiktok.com/oembed/?format=json&url=${url}`).then(res => res.json())
+            return fetch(`https://www.tiktok.com/oembed/?format=json&url=${url}`).then(res => res.json())
         },
         enabled: !!url
     })
